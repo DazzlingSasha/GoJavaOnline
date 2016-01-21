0e91151f9b0665403c1906e460195f358a3262c8
Aleksandr Panadiy <dazzlingl90@gmail.com>
2016-01-21 17:46:58 +0200

    commit for homework 10

---
+++ ModuleLesson10/SaveAndEncryptOrLoadAndDecryptFile.java
@@ -0,0 +1,56 @@
+package ModuleLesson10;
+
+
+import ModuleLesson9.EncryptAndDecryptTheText;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class SaveAndEncryptOrLoadAndDecryptFile {
+    public void saveEncryptFile(String file) {
+        EncryptAndDecryptTheText encryptDecrypt = new EncryptAndDecryptTheText();
+        File pathOnFile = new File(file);
+        try(final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            final Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter text for encryption and the file is saved in the directors : "+pathOnFile.getAbsolutePath());
+            String text = scanner.nextLine();
+
+            do {
+                out.write(encryptDecrypt.encrypt(text+'\n'));
+                text = scanner.nextLine();
+
+            } while (!text.equals("exit"));
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Do not enter a file name or path");
+                    e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error Input/Output ");
+            e.printStackTrace();
+        }
+    }
+    public void loadDecryptFile(String file) {
+        EncryptAndDecryptTheText encryptDecrypt = new EncryptAndDecryptTheText();
+
+        try(final BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+
+            while (in.ready()) {
+                System.out.println(encryptDecrypt.decrypt(in.readLine()));
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Do not enter a file name or path");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Error Input/Output ");
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        final String file = "c:\\111.txt";
+        SaveAndEncryptOrLoadAndDecryptFile s = new SaveAndEncryptOrLoadAndDecryptFile();
+        s.saveEncryptFile(file);
+        s.loadDecryptFile(file);
+
+    }
+}
