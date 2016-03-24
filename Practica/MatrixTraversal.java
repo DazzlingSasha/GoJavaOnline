df318603eb36efb9ddbf513a2e327ddfc1010fa9
Aleksandr Panadiy <dazzlingl90@gmail.com>
2016-03-24 11:17:40 +0200

    the finish matrix

--- Practica/MatrixTraversal.java
+++ Practica/MatrixTraversal.java
@@ -1,161 +1,73 @@
 package Practica;
 
 public class MatrixTraversal {
-    //    public static int[] print(int[][] input) {
-//        int width = input[0].length;
-//        int height = input.length;
-//
-//        int[] arr = new int[width * height];
-//
-//        int index = 0;
-//        int start = 0;
-//        int startH = 0;
-//
-//
-//        while (index != width * height) {
-//            if (width == 1 && height == 1) {
-//                arr[index++] = input[start][startH];
-//            } else if ((width - start - 1) == 1 && (height - startH - 1) == 1 && width > 2 && height > 2) {
-//                arr[index++] = input[start][startH];
-//            } else if (width - start - 1 == 1 && height - startH - 1 == 2) {
-//                arr[index++] = input[start][start];
-//                arr[index++] = input[start + 1][start];
-//            } else if (width - start - 1 == 2 && height - startH - 1 == 1) {
-//                arr[index++] = input[start][start];
-//                arr[index++] = input[start][start + 1];
-//            } else {
-//                for (int i = start; i < width - startH - 1; i++) { //for (int i = 0; i < width - 1; i++) i=1 width - 2
-//                    arr[index++] = input[start][i];
-//                }
-//                for (int i = start; i < height - startH - 1; i++) {//for (int j = 0; j < height - 1; j++)  i=1 height - 2
-//                    arr[index++] = input[i][width - startH - 1];
-//                }
-//
-//                for (int i = width - startH - 1; i > start; i--) {//for (int i = width - 1; i > 0; i--)  i = width - 2 > 1
-//                    arr[index++] = input[height - startH - 1][i];
-//                }
-//                for (int i = height - startH - 1; i > start; i--) {//for (int i = height - 1; i > 0; i--)  i= height -2 > 1
-//                    arr[index++] = input[i][start];
-//                }
-//            }
-//            start++;
-//            startH++;
-//        }
-//
-//        return arr;
-//    }
+    public int[] print1(int[][] input) {
 
-    static int index = 0;
-    static int startW = 0;
-    static int startH = 0;
-    static int width1 = 0;
-    static int value = 1;
-    static int start = 0;
-    static int end = 0;
-
-    public static int[] print1(int[][] input) {
         int width = input[0].length;
         int height = input.length;
+        int lengthArray = width * height;
+        int index = 0;
+        int startWidth = 0;
+        int startHeight = 0;
+        int endWidth = width - 1;
+        int endHeight = height - 1;
 
-        int[] arr = new int[width * height];
+        int[] arr = new int[lengthArray];
 
+        while (lengthArray > index) {
 
-//        System.out.println("width: " + width + ". height: " + height);
-        for (int i = 0; i < width * height; i++) {
+            if (lengthArray > index) {
+                for (int i = startWidth; i <= endWidth; i++) {
+                    arr[index++] = input[startHeight][i];
 
-            switch (value) {
-                case 1:
-                    if (i < width) {
-                        arr[i] = input[startH][startW++];
-                    } else {
-                        if (height > 2) {
-                            value = 2;
-                            startW--;
-                        } else {
-                            value = 3;
-                            startH++;
-                            startW--;
-                        }
+                    if (i == endWidth) {
+                        endWidth = startWidth;
+                        startWidth = i;
                     }
-                    break;
-                case 2:
-                    startH++;
-                    if (startH < height) {
-                        arr[i] = input[startH][startW];
-                    } else {
-                        value = 3;
-                    }
-                    break;
-                case 3:
-                    if (startW >= 0 ) {
-                        arr[i] = input[startH][startW--];
-                    } else {
-                        value = 4;
-                    }
-                    break;
-                case 4:
-                    if (startH >= 1) {
-                        arr[i] = input[startH--][startW];
-                    }
-                    break;
+                }
             }
 
+            if (height > 2 && lengthArray > index) {
+                startHeight++;
+                for (int i = startHeight; i < endHeight; i++) {
+                    arr[index++] = input[i][startWidth];
 
+                    if (i == endHeight - 1) {
+                        endHeight = startHeight;
+                        startHeight = i;
+                    }
+                }
+            }
 
+            startHeight++;
+            if (lengthArray > index) {
+                for (int i = startWidth; i >= endWidth; i--) {
+                    arr[index++] = input[startHeight][i];
 
+                    if (i == endWidth) {
+                        endWidth = startWidth;
+                        startWidth = i;
+                    }
+                }
+            }
 
+            if (height > 2 && lengthArray > index) {
+                startHeight--;
+                for (int i = startHeight; i >= endHeight; i--) {
+                    arr[index++] = input[i][startWidth];
+
+                    if (i == endHeight) {
+                        endHeight = startHeight;
+                        startHeight = i;
+                    }
+                }
+            }
+            startWidth++;
+            endWidth--;
+            height = height - 2;
 
         }
-//        if (height == 1 && width == 0) {
-//            System.out.println("null");
-//        }
-//        if (height == 1 && width == 1) {
-//            arr[index++] = input[startH][startW];
-//            System.out.println(arr[index - 1]);
-//        }
-//        if (height == 1 && width == 2) {
-//            for (int i = startW; i < input[startH].length; i++) {
-//                arr[index++] = input[startH][i];
-//            }
-//        }
-//        if (height == 2 && width == 1) {
-//            for (int i = startH; i < input.length; i++) {
-//                arr[index++] = input[i][startW];
-//            }
-//        }
-//        if (height == 2 && width >= 2) {
-//            for (int i = startW; i < input[startH].length; i++) {
-//                arr[index++] = input[startH][i];
-//            }
-//            startH++;
-//            for (int k = input[startH].length-1; k >= startW; k--) {
-//                arr[index++] = input[startH][k];
-//            }
-//        }
-//
-//        if (height >= 3 && width == 2) {
-//            for (int i = startW; i < input[startH].length; i++) {
-//                arr[index++] = input[startH][i];
-//            }
-//            startH++;
-//            for (int k = input[startH].length-1; k >= startW; k--) {
-//                arr[index++] = input[startH][k];
-//            }
-//        }
-
         return arr;
     }
 
-    public static void main(String[] args) {
-        int[][] arr = {{1, 5}, {2, 5}};
-//        for (int d : arr[0]){
-//            System.out.println(arr.length + " d" + arr[0].length+" --->"+ d);
-//        }
-//        for (int d : arr[1]){
-//            System.out.println(arr.length + " d" + arr[0].length+" --->"+ d);
-//        }
-//        System.out.println(arr[0][0]);
-        System.out.println(print1(arr));
-
-    }
 }
--- Practica/MatrixTraversalTest.java
+++ Practica/MatrixTraversalTest.java
@@ -18,28 +18,30 @@ public class MatrixTraversalTest {
     public void testPrintZero() throws Exception {
         int[][] arr = {{}};
         int[] actual = {};
-        int[] expected = MatrixTraversal.print1(arr);
+        int[] expected = matrix.print1(arr);
         assertArrayEquals(actual, expected);
     }
     @Test
     public void testPrintOne() throws Exception {
         int[][] arr = {{1}};
         int[] actual = {1};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintOneOne() throws Exception {
         int[][] arr = {{1}, {2}};
         int[] actual = {1, 2};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        int[] expected = matrix.print1(arr);
+
+        assertArrayEquals(actual, expected);
     }
 
     @Test
     public void testPrintOneTwo() throws Exception {
         int[][] arr = {{2, 4}};
         int[] actual = {2, 4};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     //    @Test
@@ -52,48 +54,54 @@ public class MatrixTraversalTest {
     public void testPrintTwoTwo() throws Exception {
         int[][] arr = {{1, 5}, {2, 5}};
         int[] actual = {1, 5, 5, 2};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintTwoThree() throws Exception {
         int[][] arr = {{1, 4}, {2, 4}, {1, 5}};
         int[] actual = {1, 4, 4, 5, 1, 2};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintThreeTwo() throws Exception {
         int[][] arr = {{1, 3, 4}, {2, 6, 7}};
         int[] actual = {1, 3, 4, 7, 6, 2};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintThreeThree() throws Exception {
         int[][] arr = {{1, 3, 4}, {2, 6, 7}, {1, 8, 9}};
         int[] actual = {1, 3, 4, 7, 9, 8, 1, 2, 6};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintThreeFore() throws Exception {
         int[][] arr = {{1, 3, 4}, {2, 6, 7}, {1, 8, 9}, {2, 6, 7}};
         int[] actual = {1, 3, 4, 7, 9, 7, 6, 2, 1, 2, 6, 8};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintForeThree() throws Exception {
         int[][] arr = {{1, 3, 4, 7}, {2, 6, 7, 1}, {1, 8, 9, 3}};
         int[] actual = {1, 3, 4, 7, 1, 3, 9, 8, 1, 2, 6, 7};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 
     @Test
     public void testPrintForeFore() throws Exception {
-        int[][] arr = {{1, 3, 4, 7}, {2, 6, 7, 1}, {1, 8, 9, 3}, {1, 4, 4, 4}};
-        int[] actual = {1, 3, 4, 7, 1, 3, 4, 4, 4, 1, 1, 2, 6, 7, 9, 8};
-        assertArrayEquals(actual, MatrixTraversal.print1(arr));
+        int[][] arr = {{1, 3, 4, 7}, {2, 6, 7, 1}, {1, 8, 9, 3}, {1, 4, 6, 4}};
+        int[] actual = {1, 3, 4, 7, 1, 3, 4, 6, 4, 1, 1, 2, 6, 7, 9, 8};
+        assertArrayEquals(actual, matrix.print1(arr));
+    }
+    @Test
+    public void testPrintFiveFive() throws Exception {
+        int[][] arr = {{1, 3, 4, 7, 8}, {2, 6, 7, 1, 9}, {1, 8, 9, 3, 8}, {1, 4, 6, 4, 6}, {1, 8, 9, 3, 8}};
+        int[] actual = {1, 3, 4, 7, 8, 9, 8, 6, 8, 3, 9, 8, 1, 1, 1, 2, 6, 7, 1, 3, 4, 6, 4, 8, 9};
+        assertArrayEquals(actual, matrix.print1(arr));
     }
 }
