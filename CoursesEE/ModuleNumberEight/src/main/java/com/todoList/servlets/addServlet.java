9a79daa817daedf94fde0272fc50505a056e54b1
Aleksandr Panadiy <dazzlingl90@gmail.com>
2016-07-26 13:03:06 +0300

    servlets todo finish (update)

--- CoursesEE/ModuleNumberEight/src/main/java/com/todoList/servlets/addServlet.java
+++ CoursesEE/ModuleNumberEight/src/main/java/com/todoList/servlets/addServlet.java
@@ -1,5 +1,7 @@
 package com.todoList.servlets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,29 +9,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 
 public class AddServlet extends HttpServlet {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(request.getParameter("name")+"..add..."+ request.getParameter("category"));
+        LOGGER.info(request.getParameter("name") + request.getParameter("category"));
 
         HttpSession session = request.getSession(true);
 
         List<Task> allTask;
         int count;
         if (session.getAttribute("task") == null){
-            System.out.println("new list");
+            LOGGER.info("new list");
             allTask = new ArrayList<>();
             count= -1;
         } else {
-            System.out.println("load list");
+            LOGGER.info("load list");
             allTask = (List<Task>) session.getAttribute("task");
             count = allTask.size()-1;
         }
@@ -42,7 +43,6 @@ public class AddServlet extends HttpServlet {
         allTask.add(task);
 
         session.setAttribute("task", allTask);
-
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
         dispatcher.forward(request, response);
